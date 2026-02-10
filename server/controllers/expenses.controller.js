f57bde3d4d75bd79c73c5d0723d794fda9c878d2
huzaifa-anw <anwarhuzaifa226@gmail.com>
2026-02-11 00:51:46 +0500

    update expense conroller

--- server/controllers/expenses.controller.js
+++ server/controllers/expenses.controller.js
@@ -1,4 +1,6 @@
+import { success } from 'zod';
 import Expense from '../models/expense.model.js'
+import expensesRouter from '../routes/expenses.routes.js';
 import { createExpenseSchema, updateExpenseSchema } from '../schemas/expenses.schema.js'
 
 export const getExpenses = async (req, res, next) => {
@@ -41,6 +43,48 @@ export const createExpense = async (req, res, next) => {
         const expense = await Expense.create({name, amount, category, note, date, userId});
         
         return res.status(201).json({success: true, msg: 'expense created', expense});
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const updateExpense = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        if(!id) {
+            const err = new Error('id not found in params');
+            err.statusCode = 400;
+            throw err;
+        }
+
+        const previousExpense = await Expense.findById(id);
+        const expenseOwnerid = previousExpense.userId;
+        const expenseId = previousExpense._id
+
+        if(!previousExpense.userId.equals(req.user._id)) {
+            const err = new Error('Cant update expenses of other users');
+            err.statusCode = 401;
+            throw err;
+        }
+
+        const validationResult = updateExpenseSchema.safeParse(req.body);
+        if(!validationResult.success) {
+            const error = new Error('Update expense input validation failed');
+            error.statusCode = 400;
+            error.issues = validationResult.error.issues;
+            throw error;
+        }
+
+        const { name, amount, date, note, category } = validationResult.data;
+
+        const updatedExpense = await Expense.findOneAndUpdate(
+            {_id: expenseId},
+            {name, amount, date, note, category, userId: expenseOwnerid},
+            {returnDocument: 'after'}
+        )
+
+        return res.status(200).json({success: true, msg: 'expense updated successfully', updatedExpense});
+
     } catch (error) {
         next(error);
     }
--- server/routes/expenses.routes.js
+++ server/routes/expenses.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import authenticate from "../middlewares/auhenticate.middleware.js";
-import { getExpenses, createExpense } from "../controllers/expenses.controller.js";
+import { getExpenses, createExpense, updateExpense } from "../controllers/expenses.controller.js";
 
 const expensesRouter = Router();
 
 expensesRouter.get('/', authenticate, getExpenses);
 expensesRouter.post('/', authenticate, createExpense );
-expensesRouter.put('/:id', authenticate, (req, res) => res.json({msg: 'update expense'}));
+expensesRouter.put('/:id', authenticate, updateExpense);
 expensesRouter.delete('/:id', authenticate, (req, res) => res.json({msg: 'delete expense'}));
 
 export default expensesRouter;
