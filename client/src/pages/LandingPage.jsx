52b2d4c39b518e1c2c913d4f24e997b3a990dfa8
huzaifa-anw <anwarhuzaifa226@gmail.com>
2026-02-14 17:43:52 +0500

    designed the landing page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,11 +1,42 @@
-function LandingPage () {
-    return (
-        <>
-            <h1 className="text-3xl text-amber-900 font-bold">
-                Landing Page
-            </h1>
-        </>
-    )
+
+function LandingPage() {
+  return (
+    <div className="bg-[url('/background.jpg')] min-h-screen bg-cover bg-center bg-no-repeat">
+        <div className="flex justify-between pl-[2vw] pr-[2vw] pt-2">
+            <img src="/spendly.png" alt="logo" className="h-[7vh]" />
+            <div className="mt-1 flex gap-4">
+                <button className="px-6 py-3 rounded-full bg-[#c8ff00] text-black font-semibold hover:cursor-pointer hover:text-white hover:bg-transparent border-[#c8ff00] border-2">
+                    Get Started
+                </button>
+
+                <button className="px-6 py-3 rounded-full text-white border-[#c8ff00] border-2 hover:cursor-pointer hover:bg-white/10">
+                    Sign In
+                </button>
+            </div>
+        </div>
+        <section className="flex flex-col items-center justify-center text-center mt-28 px-6">
+          <h2 className="text-6xl font-semibold text-white font-sans leading-tight">
+            Track expenses. <br />
+            Control your money.
+          </h2>
+
+          <p className="mt-6 font-sans max-w-xl text-white/70 text-lg">
+            Spendly helps you manage spending, analyze habits,
+            and stay financially confident.
+          </p>
+
+          <div className="mt-8 flex gap-4">
+            <button className="px-6 py-3 rounded-full bg-[#c8ff00] text-black font-semibold hover:cursor-pointer hover:text-white hover:bg-transparent border-[#c8ff00] border-2">
+                Get Started
+            </button>
+
+            <button className="px-6 py-3 rounded-full text-white border-[#c8ff00] border-2 hover:cursor-pointer hover:bg-white/10">
+                Sign In
+            </button>
+          </div>
+        </section>
+    </div>
+  );
 }
 
 export default LandingPage;
